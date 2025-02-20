2ff9d88e3b7907636d73d616a49033e2a5d31468
expired-breakfast <feng72se@outlook.com>
2025-02-20 16:40:16 +0800

    数据库提交测试

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # 使用 MySQL 数据库
+        'NAME': 'myblog',                     # 数据库名称
+        'USER': 'root',                       # 数据库用户名
+        'PASSWORD': 'newland',                # 数据库密码
+        'HOST': '172.0.0.1',                  # 数据库主机地址
+        'PORT': '3306',                       # 数据库端口号
     }
 }
 
